0d573cdf408fe360be7813c3edfcfc78a69d7ccf
Isaias de Sousa Barbosa Junior <isaias_junior@Isaiass-MacBook-Pro.local>
2019-11-25 15:29:27 -0300

    älteracao em pointers_3

--- pointer_test3/pointer_test3/main.c
+++ pointer_test3/pointer_test3/main.c
@@ -6,11 +6,21 @@ void change_value(int *p) {
     //printf("The address of x inside of function: %p\n", &x);
 }
 
+void change_value_again(int *q) {
+    *q = 150;
+    //printf("The address of x inside of function: %p\n", &x);
+}
+
+
 int main() {
     int x = 10;
     
     printf("The value of x: %d\n", x);
     change_value(&x);
     printf("The value of x: %d\n", x);
+    change_value_again(&x);
+    printf("The value of x: %d\n", x);
     return 0;
+    
+    //a change was made to this code in order to check the concept of branchs and operations on those in git
 }
